48d93fcddd70089247c49cc110563c90d9d876ff
Damien Murphy <damien.murphy@deepgram.com>
2024-10-16 12:49:47 -0700

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -7,193 +7,6 @@ const simli_faceid = '95708b15-bcb8-4d40-a4c5-b233778858b4';
 
 const simliClient = new SimliClient();
 
-export const useWebSocket = (url: string) => {
-  const [message, setMessage] = useState<string | null>(null);
-  const [isConnected, setIsConnected] = useState(false);
-  const socketRef = useRef<WebSocket | null>(null);
-  const [isRecording, setIsRecording] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
-
-  const blobToUint8Array = (blob: Blob): Promise<Uint8Array> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      
-      reader.onloadend = () => {
-        if (reader.error) {
-          reject(reader.error);
-        } else {
-          // Assert that reader.result is an ArrayBuffer
-          resolve(new Uint8Array(reader.result as ArrayBuffer));
-        }
-      };
-      
-      reader.readAsArrayBuffer(blob);
-    });
-  };
-
-  function convertAudio(audioData) {
-    // See https://stackoverflow.com/a/61481513 for tips on smooth playback
-  
-    const audioDataView = new Int16Array(audioData);
-  
-    if (audioDataView.length === 0) {
-      console.error("Received audio data is empty.");
-      return;
-    }
-
-    const audioContext = new (window.AudioContext)({ latencyHint: "interactive", sampleRate: 48000 });
-  
-    // 1 channel, 48 kHz sample rate
-    const audioBuffer = audioContext.createBuffer(
-      1,
-      audioDataView.length,
-      48000
-    ); 
-    const audioBufferChannel = audioBuffer.getChannelData(0);
-  
-    // Copy audio data to the buffer
-    for (var i = 0; i < audioDataView.length; i++) {
-      // Convert linear16 PCM to float [-1, 1]
-      audioBufferChannel[i] = audioDataView[i] / 32768; 
-    }
-    return audioBufferChannel;
-  }
-
-  const startWebSocket = () => {
-    console.log('WebSocket connecting');
-    socketRef.current = new WebSocket(url);
-    socketRef.current.binaryType = 'arraybuffer';
-
-    socketRef.current.onopen = () => {
-      setIsConnected(true);
-      console.log('WebSocket connected');
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const constraints = {
-          audio: {
-            sampleRate: 16000,
-            channelCount: 1,
-            echoCancellation: true,
-            autoGainControl: true,
-            voiceIsolation: true,
-            noiseSuppression: false,
-            latency: 0,
-          },
-        };
-        navigator.mediaDevices.getUserMedia(constraints)
-          .then((stream) => {
-            const audioContext = new AudioContext();
-            const microphone = audioContext.createMediaStreamSource(stream);
-            const processor = audioContext.createScriptProcessor(4096, 1, 1);
-
-            processor.onaudioprocess = function (event) {
-              const inputData = event.inputBuffer.getChannelData(0);
-              const rms = Math.sqrt(
-                inputData.reduce((sum, value) => sum + value * value, 0) /
-                inputData.length
-              );
-              var downsampledData = downsample(inputData, 48000, 16000);
-              sendAudioData(convertFloat32ToInt16(downsampledData));
-            };
-
-            microphone.connect(processor);
-            processor.connect(audioContext.destination);
-          })
-          .catch((error) => console.error('Error accessing microphone:', error));
-      } else {
-        mediaRecorder?.stop();
-      }
-    };
-
-    socketRef.current.onmessage = (event) => {
-      console.log('socketRef onmessage', event.data);
-      if (typeof event.data === "string") {
-        console.log("Text message received:", event.data);
-      } else if (event.data instanceof ArrayBuffer) {
-        console.log('pcm16Data', event.data);
-
-        const chunkSize = 6000;
-        for (let i = 0; i < event.data.byteLength; i += chunkSize) {
-          const chunk = event.data.slice(i, i + chunkSize);
-          const uint8Array = new Uint8Array(chunk);
-          simliClient.sendAudioData(uint8Array);
-        }
-      } else {
-        console.log('Received message:', event.data);
-      }
-    };
-
-    socketRef.current.onclose = () => {
-      setIsConnected(false);
-      console.log('WebSocket disconnected');
-    };
-
-    socketRef.current.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
-  };
-
-  function downsample(buffer, fromSampleRate, toSampleRate) {
-    if (fromSampleRate === toSampleRate) {
-      return buffer;
-    }
-    var sampleRateRatio = fromSampleRate / toSampleRate;
-    var newLength = Math.round(buffer.length / sampleRateRatio);
-    var result = new Float32Array(newLength);
-    var offsetResult = 0;
-    var offsetBuffer = 0;
-    while (offsetResult < result.length) {
-      var nextOffsetBuffer = Math.round(
-        (offsetResult + 1) * sampleRateRatio
-      );
-      var accum = 0, count = 0;
-      for (
-        var i = offsetBuffer;
-        i < nextOffsetBuffer && i < buffer.length;
-        i++
-      ) {
-        accum += buffer[i];
-        count++;
-      }
-      result[offsetResult] = accum / count;
-      offsetResult++;
-      offsetBuffer = nextOffsetBuffer;
-    }
-    return result;
-  }
-  
-  function convertFloat32ToInt16(buffer) {
-    var l = buffer.length;
-    var buf = new Int16Array(l);
-    while (l--) {
-      buf[l] = Math.min(1, buffer[l]) * 0x7fff;
-    }
-    return buf.buffer;
-  }
-
-  const sendMessage = (msg: string) => {
-    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
-      socketRef.current.send(msg);
-    }
-  };
-
-  const sendAudioData = (audioData: ArrayBuffer) => {
-    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
-      socketRef.current.send(audioData);
-    } else {
-      console.log('socket not open', audioData);
-    }
-  };
-
-  // Close the WebSocket connection when the component is unmounted
-  useEffect(() => {
-    return () => {
-      socketRef.current?.close();
-    };
-  }, []);
-
-  return { message, isConnected, sendMessage, sendAudioData, startWebSocket };
-};
-
 const Demo = () => {
   const [inputText, setInputText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
@@ -203,6 +16,193 @@ const Demo = () => {
   const audioContext = useRef<AudioContext | null>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
+
+  const useWebSocket = (url: string) => {
+    const [message, setMessage] = useState<string | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const socketRef = useRef<WebSocket | null>(null);
+    const [isRecording, setIsRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+  
+    const blobToUint8Array = (blob: Blob): Promise<Uint8Array> => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        
+        reader.onloadend = () => {
+          if (reader.error) {
+            reject(reader.error);
+          } else {
+            // Assert that reader.result is an ArrayBuffer
+            resolve(new Uint8Array(reader.result as ArrayBuffer));
+          }
+        };
+        
+        reader.readAsArrayBuffer(blob);
+      });
+    };
+  
+    function convertAudio(audioData) {
+      // See https://stackoverflow.com/a/61481513 for tips on smooth playback
+    
+      const audioDataView = new Int16Array(audioData);
+    
+      if (audioDataView.length === 0) {
+        console.error("Received audio data is empty.");
+        return;
+      }
+  
+      const audioContext = new (window.AudioContext)({ latencyHint: "interactive", sampleRate: 48000 });
+    
+      // 1 channel, 48 kHz sample rate
+      const audioBuffer = audioContext.createBuffer(
+        1,
+        audioDataView.length,
+        48000
+      ); 
+      const audioBufferChannel = audioBuffer.getChannelData(0);
+    
+      // Copy audio data to the buffer
+      for (var i = 0; i < audioDataView.length; i++) {
+        // Convert linear16 PCM to float [-1, 1]
+        audioBufferChannel[i] = audioDataView[i] / 32768; 
+      }
+      return audioBufferChannel;
+    }
+  
+    const startWebSocket = () => {
+      console.log('WebSocket connecting');
+      socketRef.current = new WebSocket(url);
+      socketRef.current.binaryType = 'arraybuffer';
+  
+      socketRef.current.onopen = () => {
+        setIsConnected(true);
+        console.log('WebSocket connected');
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          const constraints = {
+            audio: {
+              sampleRate: 16000,
+              channelCount: 1,
+              echoCancellation: true,
+              autoGainControl: true,
+              voiceIsolation: true,
+              noiseSuppression: false,
+              latency: 0,
+            },
+          };
+          navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+              const audioContext = new AudioContext();
+              const microphone = audioContext.createMediaStreamSource(stream);
+              const processor = audioContext.createScriptProcessor(4096, 1, 1);
+  
+              processor.onaudioprocess = function (event) {
+                const inputData = event.inputBuffer.getChannelData(0);
+                const rms = Math.sqrt(
+                  inputData.reduce((sum, value) => sum + value * value, 0) /
+                  inputData.length
+                );
+                var downsampledData = downsample(inputData, 48000, 16000);
+                sendAudioData(convertFloat32ToInt16(downsampledData));
+              };
+  
+              microphone.connect(processor);
+              processor.connect(audioContext.destination);
+            })
+            .catch((error) => console.error('Error accessing microphone:', error));
+        } else {
+          mediaRecorder?.stop();
+        }
+      };
+  
+      socketRef.current.onmessage = (event) => {
+        console.log('socketRef onmessage', event.data);
+        if (typeof event.data === "string") {
+          console.log("Text message received:", event.data);
+        } else if (event.data instanceof ArrayBuffer) {
+          console.log('pcm16Data', event.data);
+  
+          const chunkSize = 6000;
+          for (let i = 0; i < event.data.byteLength; i += chunkSize) {
+            const chunk = event.data.slice(i, i + chunkSize);
+            const uint8Array = new Uint8Array(chunk);
+            simliClient.sendAudioData(uint8Array);
+          }
+        } else {
+          console.log('Received message:', event.data);
+        }
+      };
+  
+      socketRef.current.onclose = () => {
+        setIsConnected(false);
+        console.log('WebSocket disconnected');
+      };
+  
+      socketRef.current.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
+    };
+  
+    function downsample(buffer, fromSampleRate, toSampleRate) {
+      if (fromSampleRate === toSampleRate) {
+        return buffer;
+      }
+      var sampleRateRatio = fromSampleRate / toSampleRate;
+      var newLength = Math.round(buffer.length / sampleRateRatio);
+      var result = new Float32Array(newLength);
+      var offsetResult = 0;
+      var offsetBuffer = 0;
+      while (offsetResult < result.length) {
+        var nextOffsetBuffer = Math.round(
+          (offsetResult + 1) * sampleRateRatio
+        );
+        var accum = 0, count = 0;
+        for (
+          var i = offsetBuffer;
+          i < nextOffsetBuffer && i < buffer.length;
+          i++
+        ) {
+          accum += buffer[i];
+          count++;
+        }
+        result[offsetResult] = accum / count;
+        offsetResult++;
+        offsetBuffer = nextOffsetBuffer;
+      }
+      return result;
+    }
+    
+    function convertFloat32ToInt16(buffer) {
+      var l = buffer.length;
+      var buf = new Int16Array(l);
+      while (l--) {
+        buf[l] = Math.min(1, buffer[l]) * 0x7fff;
+      }
+      return buf.buffer;
+    }
+  
+    const sendMessage = (msg: string) => {
+      if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+        socketRef.current.send(msg);
+      }
+    };
+  
+    const sendAudioData = (audioData: ArrayBuffer) => {
+      if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+        socketRef.current.send(audioData);
+      } else {
+        console.log('socket not open', audioData);
+      }
+    };
+  
+    // Close the WebSocket connection when the component is unmounted
+    useEffect(() => {
+      return () => {
+        socketRef.current?.close();
+      };
+    }, []);
+  
+    return { message, isConnected, sendMessage, sendAudioData, startWebSocket };
+  };
 
   const { message, isConnected, sendMessage, sendAudioData, startWebSocket } = useWebSocket('ws://localhost:3001');
 
